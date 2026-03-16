8d7ef1222d59ec319cfc2c60831b00a5ecbaefdb
BadRobotXIII <154725144+BadRobotXIII@users.noreply.github.com>
2026-03-16 00:37:24 -0700

    Started adding navigation animations, chevron rotation sub menu hiding

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,5 +8,19 @@ document.getElementById("btn-home")?.addEventListener("click", () =>{
     console.log("Going Home")
     window.location.href = "index.html"
 });
-        
-// document.getElementById()
+
+document.querySelectorAll('.trails-menu-1').forEach(item => {
+  item.addEventListener('click', () => {
+    const parent = item.closest('.trails-menu-1');
+    console.log(parent);
+    parent?.classList.toggle('open');
+  });
+}); 
+
+document.querySelectorAll('.trails-menu-2').forEach(item => {
+  item.addEventListener('click', () => {
+    const parent = item.closest('.trails-menu-2');
+    console.log(parent);
+    parent?.classList.toggle('open');
+  });
+}); 
